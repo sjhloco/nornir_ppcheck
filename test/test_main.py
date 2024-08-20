fa29058dc26be82482185042fbfa4e68b15bd786
mucholoco <mucholoco@hotmail.co.uk>
2024-08-20 09:53:38 +0100

    test(): Added unit tests for err_missing_files and val_input_file methods from the TestInputValidate class

--- test/test_main.py
+++ test/test_main.py
@@ -83,69 +83,46 @@ class TestInputValidate:
         err_msg = "❌ dir_exist_get_paths: Output folder creation failed"
         assert os.path.exists(output_fldr) == True, err_msg
 
-
-
-    # 1b. Testing method for raising error if files missing  (input or compare)
+    # 1b. Testing method for raising error if files missing (input or compare)
     def test_err_missing_files(self, capsys):
-
-    # ----------------------------------------------------------------------------
-    # ERR_MISS: Errors and exits if files are missing (input or compare)
-    # ----------------------------------------------------------------------------
-    def err_missing_files(self, run_type: str, missing_files: list) -> None:
-        if len(missing_files) != 0:
-            files = ", ".join(missing_files)
-            self.rc.print(f":x: The '{run_type}' file {files} does not exist")
-            sys.exit(1)
-
+        err_msg = "❌ err_missing_files: Missing file raise error test failed"
+        desired_result = f"❌ The 'cmp' file test_file1, test_file2 does not exist"
+        try:
+            input_val.err_missing_files("cmp", ["test_file1", "test_file2"])
+        except SystemExit:
+            pass
+        assert capsys.readouterr().out.replace("\n", "") == desired_result, err_msg
 
     # 1c. Testing method for raising error if input file contents are not in the correct format
-    # def test_val_input_file(self, capsys):
-
-
-
-        # test creating folder
-
-        # err_msg = "❌ dir_exist_get_paths: Input file load check failed"
-        # desired_result = {
-        #     "input_data": {
-        #         "all": {"cmd_print": ["show etherchannel summary"]},
-        #         "groups": {"ios": {"cmd_print": ["show clock"]}},
-        #         "hosts": {"HME-SWI-VSS01": {"cmd_print": ["show ip ospf neighbor"]}},
-        #     },
-        #     "input_file": "/Users/mucholoco/Documents/Coding/Nornir/code/nornir_checks/test/test_files/input_cmd.yml",
-        #     "output_fldr": None,
-        # }
-        # test_file = os.path.join(input_fldr, "input_cmd.yml")
-        # actual_result = input_val.dir_file_exist("print", test_file, "test.yml")
-        # assert actual_result == desired_result, err_msg
-
-    # ----------------------------------------------------------------------------
-    # INPUT_FILE: Validates input files contents are of the correct format
-    # ----------------------------------------------------------------------------
-    def val_input_file(
-        self, run_type: str, input_file: str, input_data: Dict[str, Any]
-    ) -> None:
-        if input_data == None:
-            self.rc.print(f":x: The '{run_type}' input file {input_file} is empty")
-            sys.exit(1)
-        elif (
-            not isinstance(input_data.get("hosts"), dict)
-            and not isinstance(input_data.get("groups"), dict)
-            and not isinstance(input_data.get("all"), dict)
-        ):
-            self.rc.print(
-                f":x: {input_file} must have at least one [i]hosts, groups[/i] or [i]all[/i] dictionary"
-            )
-            sys.exit(1)
-
-            
-
-
-
-
-
-
- 
+    def test_val_input_file(self, capsys):
+        # Test for empty input file
+        err_msg = "❌ val_input_file: Test raising error if input file is empty"
+        desired_result = f"❌ The 'pos' input file {input_file} is empty"
+        try:
+            input_val.val_input_file("pos", input_file, None)
+        except SystemExit:
+            pass
+        assert capsys.readouterr().out.replace("\n", "") == desired_result, err_msg
+        # Test for input file without a hosts, groups or all dictionary
+        err_msg = "❌ val_input_file: Test raising error if input file doesn't have one of hosts, groups or all dicts"
+        desired_result = (
+            f"❌ {input_file} must have at least one hosts, groups or all dictionary"
+        )
+        try:
+            input_val.val_input_file("pos", input_file, {"hst": {}, "grp": {}, "a": {}})
+        except SystemExit:
+            pass
+        assert capsys.readouterr().out.replace("\n", "") == desired_result, err_msg
+        # Test for input file without hosts, groups or all holding a dictionary
+        err_msg = "❌ val_input_file: Test raising error if input file doesn't have one of hosts, groups or all dicts"
+        desired_result = (
+            f"❌ {input_file} must have at least one hosts, groups or all dictionary"
+        )
+        try:
+            input_val.val_input_file("pos", input_file, {"hosts": []})
+        except SystemExit:
+            pass
+        assert capsys.readouterr().out.replace("\n", "") == desired_result, err_msg
 
     # 1d. Testing method for adding extra arguments to argparser
     def test_add_arg_parser(self):
@@ -173,10 +150,77 @@ class TestInputValidate:
         }
         assert actual_result == desired_result, err_msg
 
+    # 1e. Testing method for getting username and password
 
-#? 1a. dir_exist_get_paths(self, run_type: str, file_path: str) -> Dict[str, Any]:
-# 1b. err_missing_files(self, run_type: str, missing_files: list) -> None:
-# 1c.  val_input_file(self, run_type: str, input_file: str, input_data: Dict[str, Any]) -> None:
+
+#     def test_get_user_pass(self):
+
+#    def get_user_pass(self, args: Dict[str, Any]) -> Dict[str, str]:
+
+#         # USER: Check for username in this order: args, env var, var, prompt
+#         device = {}
+#         if args.get("username") != None:
+#             device["user"] = args["username"]
+#         elif os.environ.get("DEVICE_USER") != None:
+#             device["user"] = os.environ["DEVICE_USER"]
+#         elif device_user != None:
+#             device["user"] = device_user
+#         else:
+#             device["user"] = input("Enter device username: ")
+#         # PWORD: Check for password in thid order: env var, prompt
+#         if os.environ.get("DEVICE_PWORD") != None:
+#             device["pword"] = os.environ["DEVICE_PWORD"]
+#         else:
+#             device["pword"] = getpass.getpass("Enter device password: ")
+#         return device
+
+# 1f. Testing method for running script to validate with compare arg
+# def test_val_compare_arg(self):
+
+# # ----------------------------------------------------------------------------
+# # 1d. If compare arg validates all the files exist (a list of 3 elements, output_dir & 2 compare files)
+# # ----------------------------------------------------------------------------
+# def val_compare_arg(self, run_type: str, file_path: str) -> Dict[str, Any]:
+#     missing_files = []
+#     # DIR: Check that output dir exists and get full file path
+#     working_dir, output_fldr, z = self.dir_exist_get_paths(run_type, file_path[0])
+#     # FILE: Check that compare files exist
+#     cmp_file1 = os.path.join(working_dir, file_path[1])
+#     if not os.path.exists(cmp_file1):
+#         missing_files.append(cmp_file1)
+#     cmp_file2 = os.path.join(working_dir, file_path[2])
+#     if not os.path.exists(cmp_file2):
+#         missing_files.append(cmp_file2)
+#     # ERR/RTR: Errors or returns file paths based on whether exist or not
+#     self.err_missing_files(run_type, missing_files)
+#     return dict(output_fldr=output_fldr, cmp_file1=cmp_file1, cmp_file2=cmp_file2)
+
+
+# # 1g. Testing method for running script to validate with any arg other than compare
+# def test_val_noncompare_arg(self):
+
+# # ----------------------------------------------------------------------------
+# # 1e. Validates input command file exists and contents are of the correct format
+# # ----------------------------------------------------------------------------
+# def val_noncompare_arg(self, run_type: str, file_path: str) -> Dict[str, Any]:
+#     # DIR: Check that output dir exists and get full file path
+#     z, output_fldr, input_file = self.dir_exist_get_paths(run_type, file_path)
+#     # FILE: Check input file exists and loads and validate contents
+#     if not os.path.exists(input_file):
+#         self.err_missing_files(run_type, [input_file])
+#     elif os.path.exists(input_file):
+#         with open(input_file, "r") as file_content:
+#             input_data = yaml.load(file_content, Loader=yaml.FullLoader)
+#         # ERR/RTR: Errors or returns file paths based on whether input file correctly formatted
+#         self.val_input_file(run_type, input_file, input_data)
+#         return dict(
+#             output_fldr=output_fldr, input_file=input_file, input_data=input_data
+#         )
+
+
+# ? 1a. dir_exist_get_paths(self, run_type: str, file_path: str) -> Dict[str, Any]:
+# ? 1b. err_missing_files(self, run_type: str, missing_files: list) -> None:
+# ? 1c.  val_input_file(self, run_type: str, input_file: str, input_data: Dict[str, Any]) -> None:
 # ? 1d. def add_arg_parser(self, nr_inv_args) -> Dict[str, Any]:
 # 1e. def get_user_pass(self, args: Dict[str, Any]) -> Dict[str, str]:
 # 1f. def val_compare_arg(self, run_type: str, file_path: str) -> Dict[str, Any]:
